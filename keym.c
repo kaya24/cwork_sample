423cd33f1b902fd2a5f263d54bf19d8d57c91d78
kaya24 <skysm1024.goo.goo@gmail.com>
2022-03-21 15:34:56 +0900

    文字化け修正（UTF-8に統一）

--- keym.c
+++ keym.c
@@ -5,15 +5,15 @@ int main()
 
     /* 問題１：KeyElemTbl_0[0]のkeyElementIdをprintfで表示するプログラムを作成してください*/
     
-    /* 問題２−１：KeyElemTbl_0[0]のkeyDataの文字列をprintfで表示するプログラムを作成してください */
+    /* 問題２-１：KeyElemTbl_0[0]のkeyDataの文字列をprintfで表示するプログラムを作成してください*/
     
-    /* 問題２−２KeyElemTbl_0[0]のkeyDataの3番目の文字だけをprintfで表示するプログラムを作成してください*/
+    /* 問題２-２KeyElemTbl_0[0]のkeyDataの3番目の文字だけをprintfで表示するプログラムを作成してください*/
     
     /* 問題３：KeyDataTbl[0]の構造体メンバから，keyElementId=0　をprintfで表示するプログラムを作成してください */
 
     /* 問題４：KeyDataTbl[0]の構造体メンバから，keyElementId=1　をprintfで表示するプログラムを作成してください */
 
-    /* 問題5：KeyDataTbl[2]の構造体メンバから，keyElementId=4 を表示するプログラムを作成してください*/
+    /* 問題５：KeyDataTbl[2]の構造体メンバから，keyElementId=4 を表示するプログラムを作成してください */
     
     
     return 0;
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
->gccでのコンパイル方法
+>gccコンパイラコマンド
 gcc -o keym_sample keym.c keym_cfg.c
 
 >実行方法
